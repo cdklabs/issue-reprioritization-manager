6ff12aa242489294b5de267952fd3bd7f5f9c09e
kaizen3031593 <zen.conroy@gmail.com>
2022-04-04 16:40:03 -0400

    new projen manager

--- .projenrc.js
+++ .projenrc.js
@@ -2,10 +2,17 @@ const { actions } = require('projen');
 const project = new actions.GitHubActionTypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'issue-graduation-manager',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  metadata: {
+    author: 'Kaizen Conroy',
+    inputs: [{
+      id: 'github-token',
+      description: 'GitHub token',
+      required: true,
+    }],
+    branding: {
+      color: 'purple',
+      icon: 'arrow-up-circle',
+    },
+  },
 });
 project.synth();
---
+++ dist/index.js
@@ -0,0 +1,27 @@
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.Hello = void 0;
+class Hello {
+    sayHello() {
+        return 'hello, world!';
+    }
+}
+exports.Hello = Hello;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsTUFBYSxLQUFLO0lBQ1QsUUFBUTtRQUNiLE9BQU8sZUFBZSxDQUFDO0lBQ3pCLENBQUM7Q0FDRjtBQUpELHNCQUlDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIEhlbGxvIHtcbiAgcHVibGljIHNheUhlbGxvKCkge1xuICAgIHJldHVybiAnaGVsbG8sIHdvcmxkISc7XG4gIH1cbn0iXX0=
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map

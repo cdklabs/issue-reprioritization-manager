83c915fd034ab7e175c12bc9f5f56477d43673d7
kaizen3031593 <zen.conroy@gmail.com>
2022-04-04 17:50:07 -0400

    fix action.yml

--- .projenrc.js
+++ .projenrc.js
@@ -4,43 +4,45 @@ const project = new actions.GitHubActionTypeScriptProject({
   name: 'issue-graduation-manager',
   metadata: {
     author: 'Kaizen Conroy',
-    inputs: [{
-      id: 'github-token',
-      description: 'GitHub token',
-      required: true,
-    }, {
-      id: 'original-label',
-      description: 'label that is candidate for graduation',
-      required: true,
-    }, {
-      id: 'new-label',
-      description: 'new label attached to all graduated issues',
-      required: true,
-    }, {
-      id: 'graduation-threshold',
-      description: 'the threshold count necessary for graduation',
-      required: false,
-      default: '20',
-    }, {
-      id: 'graduation-message',
-      description: 'display this message on issues that graduate',
-      required: false,
-      default: [
-        'This issue has received a significant amount of attention',
-        'so we are automatically upgrading its priority.',
-        'A member of the community will see the re-prioritization',
-        'and update the issue accordingly.',
-      ].join(' '),
-    }, {
-      id: 'omit-message',
-      description: 'set this flag if you do not want automatic messages on graduated issues',
-      required: false,
-      default: false,
-    }],
-    outputs: [{
-      id: 'num-graduated',
-      description: 'returns the number of graduated issues',
-    }],
+    inputs: {
+      'github-token': {
+        description: 'GitHub token',
+        required: true,
+      },
+      'original-label': {
+        description: 'label that is candidate for graduation',
+        required: true,
+      },
+      'new-label': {
+        description: 'new label attached to all graduated issues',
+        required: true,
+      },
+      'graduation-threshold': {
+        description: 'the threshold count necessary for graduation',
+        required: false,
+        default: '20',
+      },
+      'graduation-message': {
+        description: 'display this message on issues that graduate',
+        required: false,
+        default: [
+          'This issue has received a significant amount of attention',
+          'so we are automatically upgrading its priority.',
+          'A member of the community will see the re-prioritization',
+          'and update the issue accordingly.',
+        ].join(' '),
+      },
+      'omit-message': {
+        description: 'set this flag if you do not want automatic messages on graduated issues',
+        required: false,
+        default: false,
+      },
+    },
+    outputs: {
+      'num-graduated': {
+        description: 'returns the number of graduated issues',
+      },
+    },
     branding: {
       color: 'purple',
       icon: 'arrow-up-circle',
